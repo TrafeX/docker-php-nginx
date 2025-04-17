0fa2952fa78fac7885fc9291b31d008697336580
promlert <Promlert_Pearrukkrai@hotmail.com>
2025-04-18 02:46:21 +0700

    icon

--- src/Views/line/deposit.php
+++ src/Views/line/deposit.php
@@ -5,9 +5,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
         <title>ฝากเงิน</title>
-
        <!-- Or for RTL support -->
        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.rtl.min.css" />
