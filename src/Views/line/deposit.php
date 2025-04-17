3039e5baeda8e78be7b5ba170a0660d23e7a2137
promlert <Promlert_Pearrukkrai@hotmail.com>
2025-04-18 02:42:54 +0700

    remove icon

--- src/Views/line/deposit.php
+++ src/Views/line/deposit.php
@@ -2,7 +2,6 @@
 <html lang="en">
     <?php $version="8";?>
     <head>
-        <link rel="icon" type="image/x-icon" href="<?php echo base_url("public/favicon.ico"); ?>">
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
